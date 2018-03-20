58decdb11d49dfd33b98bc33aabe8c21d2a56096
Black369Ace <black369ace@gmail.com>
2018-03-21 10:09:49 +1100

    Project

---
+++ TeachHighSchool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOExercise1week2
+{
+    class TeachHighSchool : ITeach
+    {
+        public string Teach()
+        {
+            return " Teach high school Brats";
+        }
+
+    }
+}
